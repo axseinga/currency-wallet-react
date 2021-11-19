c58416f0d45990aed61b79fa2d5c57322023edf8
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-11-19 17:59:29 +0000

    add save and load from local storage to redux

--- src/modules/wallet/wallet.reducer.js
+++ src/modules/wallet/wallet.reducer.js
@@ -1,24 +1,5 @@
 const initState = {
-    data: [
-        {
-            curr: "USD",
-            amount: 100,
-            purchaseDate: "2021-01-01",
-            purchasePrice: 3.74,
-        },
-        {
-            curr: "EUR",
-            amount: 300,
-            purchaseDate: "2021-01-03",
-            purchasePrice: 4.48,
-        },
-        {
-            curr: "PLN",
-            amount: 600,
-            purchaseDate: "2021-01-04",
-            purchasePrice: 1.7,
-        },
-    ],
+    data: [],
 };
 
 const walletReducer = (state = initState, action) => {
--- src/store.js
+++ src/store.js
@@ -2,6 +2,34 @@ import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./modules/reducers";
 
-const store = createStore(reducers, composeWithDevTools());
+const store = createStore(
+    reducers,
+    loadFromLocalStorage(),
+    composeWithDevTools()
+);
+
+const state = store.getState();
+
+const saveToLocalStorage = (purchasesState) => {
+    try {
+        const purchases = JSON.stringify(purchasesState);
+        localStorage.setItem("purchases", purchases);
+    } catch (error) {
+        console.log("data was not saved");
+    }
+};
+
+function loadFromLocalStorage() {
+    try {
+        const purchases = localStorage.getItem("purchases");
+        if (purchases === null) return undefined;
+        return JSON.parse(purchases);
+    } catch (e) {
+        console.log("something went wrong");
+        return undefined;
+    }
+}
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
